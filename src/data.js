972c004f1e6a1600dfed1550654f3c7d81a86616
iosebi <nebieridze123456789@gmail.com>
2022-10-09 16:31:39 +0400

    changes

--- src/data.js
+++ src/data.js
@@ -2,7 +2,10 @@ import img from "./img/img1.jpg";
 import { default as bodilava} from "./img/products/bodilava.svg";
 import { default as kombo } from "./img/products/kombo.svg";
 import { default as magic } from "./img/products/magic.svg";
-
+import { default as  lipoil} from "./img/products/lipoil.svg";
+import { default as auto } from "./img/products/auto.svg";
+import { default as sss } from "./img/products/sss.svg";
+import { default as glove } from "./img/products/glove.svg"
 
 const data = {
   productData:  [
@@ -19,7 +22,7 @@ const data = {
       
       id: 2,
       logo: kombo,
-      title: "პლაჟის კომბო",
+      title: "Beach combo",
       desc: "",
       price: 100,
     
@@ -37,8 +40,35 @@ const data = {
     
     {
       id: 2,
-      logo: magic,
-      title: "Magic moisturizer",
+      logo: lipoil,
+      title: "lips oil",
+      desc: "",
+      price: 100,
+      
+    },
+
+    {
+      id: 2,
+      logo: auto,
+      title: "Self-tanning spray",
+      desc: "",
+      price: 100,
+      
+    },
+
+    {
+      id: 2,
+      logo: sss,
+      title: "Rougelighter",
+      desc: "",
+      price: 100,
+      
+    },
+   
+    {
+      id: 2,
+      logo: glove,
+      title: "Self Tanning Glove",
       desc: "",
       price: 100,
       
